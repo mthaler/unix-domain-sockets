aefb0bc64b48e21f27f34339cd9f9fc4c8830e79
Michael Thaler <michael.thaler@dlr.de>
2021-05-01 10:22:32 +0200

    Updated Main.java.

--- src/main/java/com/mthaler/unixdomainsockets/Main.java
+++ src/main/java/com/mthaler/unixdomainsockets/Main.java
@@ -3,7 +3,9 @@ package com.mthaler.unixdomainsockets;
 import java.io.IOException;
 import java.net.StandardProtocolFamily;
 import java.net.UnixDomainSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -13,5 +15,28 @@ public class Main {
         Path socket = Paths.get("/tmp/uds.socket");
         UnixDomainSocketAddress address = UnixDomainSocketAddress.of(socket);
         ServerSocketChannel serverChannel = ServerSocketChannel.open(StandardProtocolFamily.UNIX);
+        serverChannel.bind(address);
+
+        Thread serverThread = new Thread(() -> {
+           try {
+               SocketChannel channel = serverChannel.accept();
+               ByteBuffer buffer = ByteBuffer.allocate(1024);
+               while (true) {
+                   int count = channel.read(buffer);
+                   if (count > 0) {
+                       byte[] bytes = new byte[count];
+                       buffer.flip();
+                       buffer.get(bytes);
+                       String msg = new String(bytes);
+                       System.out.println(msg);
+                   }
+                   buffer.clear();
+                   Thread.sleep(500);
+               }
+           } catch (IOException | InterruptedException ex) {
+               System.err.println("Server error " + ex);
+           }
+        });
+        serverThread.start();
     }
 }
